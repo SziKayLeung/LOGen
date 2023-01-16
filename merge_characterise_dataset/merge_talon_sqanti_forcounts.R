d15a115d779bb370f9619a4e3043cfc4c4f9df1d
sl693 <sl693@exeter.ac.uk>
2023-01-16 17:47:39 +0000

    add merge_talon_sqanti_forcounts.R

---
+++ merge_characterise_dataset/merge_talon_sqanti_forcounts.R
@@ -0,0 +1,65 @@
+## ---------- Script -----------------
+##
+## Script name: merge_talon_sqanti_forcounts.R
+##
+## Purpose of script: Include the counts from the TALON abundance file into the sqanti classification file for downstream processing
+##
+## Author: Szi Kay Leung
+##
+## Email: S.K.Leung@exeter.ac.uk
+##
+## ---------- Notes -----------------
+## Requires a metadata phenotype file (Tab delimitied), with 4 columns: Sample, Condition, ID, SQ_ID
+##    SQ_ID has to match the column names of the samples in the TALON abundance file
+## Output:
+##    Generates a sqanti classification file with the counts from TALON matched by isoform ID
+
+
+## ---------- packages -----------------
+
+suppressMessages(library("dplyr"))
+suppressMessages(library("tibble"))
+suppressMessages(library("optparse"))
+
+
+## ---------- arguments -----------------
+
+option_list <- list( 
+  make_option(c("-f", "--file"), type="character", default=NULL, 
+              help="Input sqanti classification file", metavar="character"),
+  make_option(c("-m", "--metadata"), type="character", default=NULL, 
+              help="Metadata file;#Sample#Condition(CasevsControl)#SQ_ID", metavar="character"),
+  make_option(c("-c", "--counts"), type="character", default=NULL, 
+              help="TALON abundance file"),
+  make_option(c("-o", "--output_dir"), type="character", default=NULL, 
+              help="Output_directory", metavar="character")
+)
+
+opt_parser = OptionParser(option_list=option_list)
+opt = parse_args(opt_parser)
+
+
+## ---------- Input files -----------------
+
+cat("Processing",opt$file,"\n")
+class.files <- read.table(opt$file, as.is = T, sep = "\t", header = T) 
+
+cat("Processing",opt$metadata,"\n")
+metadata <- read.table(opt$metadata, as.is = T, header = T, sep = "\t")
+
+cat("Processing",opt$counts,"\n")
+counts <- read.table(opt$counts, as.is = T, header = T, sep = "\t")
+
+
+## ---------- Merge classification and counts file -----------------
+
+# subset the TALON counts file by the counts of the samples of interest
+subsetted_counts <- counts %>% select(annot_transcript_id, metadata$SQ_ID)
+
+# merge the original classification file with the samples of interest
+# only keep the counts from the isoforms in the classification file (as final, filtered file)
+class.files.merged <- merge(class.files, subsetted_counts, by.x = "isoform", by.y = "annot_transcript_id", all.x = T)
+
+# write output file
+basename = tools::file_path_sans_ext(basename(opt$file))
+write.table(class.files.merged, paste0(opt$output_dir,"/",basename,"_counts.txt"), sep = "\t", quote = F, row.names = F)
