a8167b75ac0e9655887396e479ba7edaa84bd6ed
sl693 <sl693@exeter.ac.uk>
2023-02-08 13:29:20 +0000

    demuliplex ont and iso-seq datasets using cupcake collapse

---
+++ assist_ont_processing/adapt_cupcake_to_ont.py
@@ -0,0 +1,169 @@
+#!/usr/bin/env python3
+
+
+"""
+Author: Szi Kay Leung (S.K.Leung@exeter.ac.uk)
+Date: Jan 2023
+Aim: generate supplement files for ont pipeline
+1. <sample>.cluster_report.csv (equivalent format to Iso-Seq pipeline) to run get_abundance_post_collapse.py
+2. <prefix>_sample_id.csv for demultplexing
+
+Example of input and output files generated:
+---- Input:
+.../Batch1/S1.fasta
+> A
+TGAT....
+> B
+AGCA....
+
+.../Batch1/S2.fasta
+> C
+TGAT....
+
+---- Output:
+.../Batch1/S1.cluster_report.csv 
+cluster_id,read_id,read_type
+transcript/A,A,FL
+transcript/B,B,FL
+
+.../Batch1/S2.cluster_report.csv 
+cluster_id,read_id,read_type
+transcript/C,C,FL
+
+.../Batch1/<all_samples>_sample_id.csv
+A,S1
+B,S1
+C,S2
+
+"""
+
+# packages
+import argparse
+import os
+import glob
+from Bio import SeqIO
+
+
+"""
+list_named_fasta
+to return specified sample.fa, sample name must in the 1st part of prefix (i.e S1_xxxx.fasta)
+:returns list: fasta files in input directory 
+"""
+def list_named_fasta(args):
+    
+    # store name into fastafiles list
+    fasta_files = []
+    for file in glob.glob(args.fasta + "/*.fa"):
+        # if argument of selected samples is specified
+        if args.samples is not None:
+            for sample in args.samples:
+                if sample in file:
+                    print("Including:", file, "\n")
+                    fasta_files.append(file)
+        else:
+            print("Including:", file, "\n")
+            fasta_files.append(file)
+    
+    return(fasta_files)
+
+
+"""
+extract_reads_output_cluster
+:params args
+:params infasta = str of input fasta file
+:params outsampledf = open <all_samples>_sample_id.csv to append read id and sample
+:writes <sample>_cluster_report.csv
+:writes <all_samples>_sample_id.csv 
+""" 
+def extract_reads_output_cluster(args, infasta, outsampledf):
+    '''
+    infasta = ../../Batch1/S1_input.fasta
+    
+    1. records read id of each infasta, and writes to:
+    <path_to_infasta>/<prefix_of_infasta>_cluster_report.csv if args.batch is off or
+    <path_to_infasta>/<dirname_of_infasta>_<prefix_of_infasta>_cluster_report.csv if args.batch is on
+    i.e. 
+    args.batch = off: out_cluster = ../../Batch1/S1_cluster_report.csv
+    args.batch = on: out_cluster = ../../Batch1/Batch1S1_cluster_report.csv
+
+    2. appends read id of each infasta to outsampledf (open file created outside of function)
+    i.e. 
+    inputfasta = ../../Batch1/S1_input.fasta
+    args.batch = off:
+        A,S1
+        B,S1
+    args.batch = on
+        A,Batch1S1
+        B,Batch1S1
+    '''
+    
+    print("Processing:", infasta)
+    fasta_sequences = SeqIO.parse(open(infasta),'fasta')
+    
+    # determine whether to include previous directory name into output
+    if args.batch:
+        print("Samples are processed in batches, including directory name in output")
+        infastadir = os.path.dirname(infasta)
+        batchname = os.path.basename(infastadir)
+        sample = batchname + os.path.basename(infasta).split("_")[0]
+    else:
+        sample = os.path.basename(infasta).split("_")[0]
+    
+    # create <sample>_cluster_report.csv
+    out_cluster_name = args.dir + "/" + sample + "_cluster_report.csv"
+    print("Writing to:", out_cluster_name,"\n")
+    out_cluster = open(out_cluster_name,"w")
+    out_cluster.write("cluster_id,read_id,read_type\n")
+    
+    # write each fasta ID in fasta sequences
+    for fasta in fasta_sequences: 
+        out_cluster.write("transcript/" + fasta.id + "," + fasta.id + ",FL\n")
+        outsampledf.write(str(fasta.id) + "," + sample + "\n")
+    
+    # close <sample>_cluster_report.csv
+    out_cluster.close()
+                
+
+"""
+extract_reads_output_sample
+:params args
+:params fastafiles = list of fasta files to record ID
+:writes <all_samples>_sample_id.csv 
+""" 
+def extract_reads_output_sample(args, fasta_files):
+    # create <all_samples>_sample_id.csv
+    out_sample_name = args.dir + "/" + args.output + "_sample_id.csv"
+    print("Writing sample id file to", out_sample_name,"\n")
+    out_sample_name_file = open(out_sample_name,'w')
+    out_sample_name_file.write("id,primer\n")
+    
+    # loop through each fasta file to record ID to one file
+    for f in fasta_files: 
+        extract_reads_output_cluster(args, f, out_sample_name_file)
+    
+    # close <all_samples>_sample_id.csv
+    out_sample_name_file.close()
+
+
+def main():
+    parser = argparse.ArgumentParser(description="Generating input files for get_abundance_post_collapse.py and demux_ont_with_genome.py")
+    parser.add_argument('fasta', help='\t\tDirectory of fasta files.')
+    parser.add_argument('-s','--samples', nargs='+', required=False, help='\t\tSpecified samples to include. Default: all files with .fa/.fasta')
+    parser.add_argument('--batch', default=False, action="store_true", help='\t\tInclude batch name into output files')
+    parser.add_argument('-o','--output', required=False, help='\t\tPrefix for output sample file documenting reads.')
+    parser.add_argument('-d','--dir', required=False, help='\t\tDirectory for output files. Default: Directory of fasta files.')
+    
+    args = parser.parse_args()
+    if args.output is None:
+        args.output = "output_sample_id" 
+    
+    if args.dir is None:
+        args.dir = args.fasta 
+        
+    fasta_files = list_named_fasta(args)
+    extract_reads_output_sample(args, fasta_files)
+    
+    print("All Done")
+    
+if __name__ == "__main__":
+  main()
---
+++ assist_ont_processing/demux_cupcake_collapse.py
@@ -0,0 +1,137 @@
+#!/usr/bin/env python3
+
+
+"""
+Author: Szi Kay Leung (S.K.Leung@exeter.ac.uk)
+Date: Jan 2023
+Aim: Uses read_stat.txt from IsoSeq3-collapse and unique ONT read ID to generate an abundance file at sample-level
+
+---- Input:
+==> <sample>.read_stat.txt = csv file documenting original reads clustered
+id,pbid
+AA,PB.1.1
+BB,PB.1.1
+transcript/1,PB.1.1
+
+arg.dataset = ont
+==> <all_sample>.sample_id.csv = csv file documenting origin of reads for all samples
+id,primer
+AA,Sample1
+BB,Sample2
+
+arg.dataset = isoseq
+==> <all_sample>.sample_id.csv = csv file documenting origin of reads for all samples
+cluster_id,id,primer
+transcript/1,mXX/ccs,Sample3
+transcript/1,mYY/ccs,Sample4
+
+---- Output:
+arg.dataset = ont
+==> demux_fl_count.csv
+        Sample1  Sample2
+PB.1.1    1        1
+
+arg.dataset = isoseq
+        Sample3  Sample4
+PB.1.1    1        1
+
+"""
+
+# packages
+import pandas as pd
+import os
+import csv
+import argparse
+import sys
+
+"""
+demux_ont
+:params args; args.read_stat & args.sample_id already read in
+:writes demux_ignored_id.txt
+:writes demux_fl_count.csv
+:return merged_tally_pivot = df of demux_fl_count.csv
+""" 
+def demux(args):
+    '''
+    1. Merge files using "id" column or "cluster_id" and "id" i.e. read ID; keeping only common reads documented in both files
+    for ONT dataset, merge using common column "id"
+    NB all reads in sample_id.csv will be read_stat.txt, as sample_id.csv is derived from the .fa that should be used for IsoSeq3 collapse
+    
+    for Iso-Seq dataset, merge using id and cluster_id
+    Iso-Seq datasets, unlike ONT datasets, processed with Iso-Seq3 cluster 
+    Iso-Seq3 cluster labels ID as "transcript/X"
+    Iso-Seq collapse with Iso-Seq datasets therefore uses Iso-Seq3 cluster ID (rather than the original ccs ID)
+    NB keep only reads in read_stat file from collapse
+    
+    2. Writes reads that have been discarded by Iso-Seq3 collapse 
+    3. Tally the number of reads across isoform and sample
+    4. Replace the sample column if --sample argument provided
+    '''
+    
+    # merge read_stat and samples files; columns to merge dependent on dataset
+    if args.dataset == "ont":
+        print("Processing ONT dataset")
+        merged = pd.merge(args.read_stat, args.sample_id, how = "outer", on = "id")
+    else:
+        print("Processing Iso-Seq dataset")
+        merged = pd.merge(args.read_stat, args.sample_id[["cluster_id","primer"]], how = "outer", left_on = "id", right_on = "cluster_id")
+    
+    # document reads that are discarded by Iso-Seq3 collapse
+    # i.e. entry in sample_id.csv but not in read_stat.txt
+    mergedna = merged[merged['pbid'].isna()]
+    
+    # replace pb.id NA values with 0 
+    merged['pbid'] = merged['pbid'].fillna(0)
+    
+    # tally and pivot 
+    merged_tally = merged.groupby(['pbid','primer']).count()
+    merged_tally = merged_tally.reset_index()
+    merged_tally_pivot = merged_tally.pivot(index="pbid", columns="primer", values="id")
+    
+    if args.sample is not None:
+        print("Replace batch and barcode names with sample names")
+        sample = pd.read_csv(args.sample, sep = "\t")
+        merged_tally_pivot.rename(columns=sample.set_index('BatchBC')['#Sample'], inplace=True)
+    
+    # replace NA with 0
+    # reset index and rename column
+    merged_tally_pivot.fillna(0, inplace=True)
+    merged_tally_pivot.reset_index(inplace=True)
+    merged_tally_pivot.rename({'pbid': 'isoform'}, axis=1, inplace=True)
+  
+    # write output
+    mergedna.to_csv(args.dir + "/" + args.output + "_ignored_id.txt", index = False)
+    output_count = args.dir + "/" + args.output + "_fl_count.csv"
+    print("Writing output files to:", output_count)
+    merged_tally_pivot.to_csv(output_count, index = False)
+    
+    return(merged_tally_pivot)
+
+
+def main():
+    parser = argparse.ArgumentParser(description="ONT demultiplex and retrieve read counts from IsoSeq3 collapse output")
+    parser.add_argument("read_stat", help='\t\tread_stat.txt generated from Cupcake/Isoseq3 collapse')
+    parser.add_argument('sample_id', help='\t\tcsv file of reads and sample; <id,primer> for ont\t <cluster_id,id,primer> for isoseq')
+    parser.add_argument('--dataset', default = "ont", choices=['ont', 'isoseq'], help='\t\tdataset:"ont" or "isoseq"')
+    parser.add_argument('-o','--output', required=False, help='\t\tPrefix for output abundance file; Default: demux_fl_count.csv.')
+    parser.add_argument('-s','--sample', required=False, default=None, help='\t\ttsv file of samples to replace <#sample> <BatchBC> (optional)')
+    parser.add_argument('-d','--dir', required=False, help='\t\tDirectory for output files. Default: Directory of read_stat.txt.')
+  
+    args = parser.parse_args()
+    if args.output is None:
+        args.output = "demux" 
+    
+    if args.dir is None:
+        args.dir =  os.path.dirname(stat)
+        
+    # read in files
+    print("Reading in files...")
+    args.read_stat = pd.read_csv(args.read_stat, sep = "\t")
+    args.sample_id = pd.read_csv(args.sample_id)
+
+    demux(args)
+    print("All Done")
+    
+
+if __name__ == "__main__":
+    main()
