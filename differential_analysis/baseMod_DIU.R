56706da5a5f02d700039dc2b00721c7cd5cdf828
SziKayLeung <33493350+SziKayLeung@users.noreply.github.com>
2024-01-23 14:13:56 +0000

    add DIU python scripts

---
+++ differential_analysis/baseMod_DIU.R
@@ -0,0 +1,157 @@
+# Differential Isoform Usage Analysis R script
+#
+# Differential Isoform Usage analysis will be performed by using two different methods:
+#   edgeR spliceVariants function and
+#   DEXSeq package
+# tappAS functions(): https://github.com/ConesaLab/tappAS/blob/master/scripts/DIU.R
+
+minorFoldfilterTappas <- function(data, gen, minorfilter, minorMethod=c("PROP","FOLD")){
+  print ("Removing low expressed minor isoforms")
+  moreOne <- names(which(table(gen) > 1))
+  head(moreOne)
+  iso.sel <- NULL
+  iso.sel.prop <- NULL
+
+  gene.sel <- NULL
+  if(minorMethod=="FOLD"){
+    for ( i in moreOne) {
+      which(gen==i)
+      gene.data <- data[which(gen==i),]
+      row.names(gene.data) <- row.names(data)[which(gen==i)]
+      # sum the number of reads for each isoform across all the samples
+      isoSUM <- apply(gene.data, 1, sum)
+      names(isoSUM) <- row.names(data)[which(gen==i)]
+      # identify major isoform as the isoform with the highest abundance
+      major <- names(which(isoSUM == max(isoSUM)))[1]
+      minors <- names(which(isoSUM != max(isoSUM)))
+      # for each minor isoform, divide major isoform FL read/minor isoform FL read
+      # i.e. major isoform = 45, minor isoform = 11, therefore fold change = 4.09
+      #div <- as.numeric(matrix(rep(gene.data[major,], length(minors)), ncol = ncol(data), length(minors), byrow = T)) / as.matrix(gene.data[minors,])
+
+      major_index <- which(row.names(gene.data) == major)
+      major_row <- gene.data[major_index, ]
+      minors_rows <- gene.data[which(row.names(gene.data) %in% minors), ]
+      div <- as.matrix(data.table::rbindlist(rep(list(major_row), length(minors))))/as.matrix(minors_rows)
+      row.names(div) <- minors
+
+      # keep if fold change < 2
+      # note if the fold change would be smaller, if the minor isoform reads are higher (as number/big number = small number)
+      is <- names(which(apply(div, 1, min, na.rm = T) < minorfilter))
+      iso.sel <- c(iso.sel, major, is)
+
+    }
+  }else{
+    for ( i in moreOne) {
+      which(gen==i)
+      gene.data <- data[which(gen==i),]
+
+      # by proportion
+      geneSUM <- apply(gene.data, 2, sum)
+      proportion = t(t(gene.data)/geneSUM)
+      is.prop = rownames(proportion[apply(proportion, 1, function(x) any(x>minorfilter)),,drop=F])
+      iso.sel <- c(iso.sel, is.prop)
+
+    }}
+
+  print(length(iso.sel))
+  print ("Done")
+  return(iso.sel)
+}
+
+### EdgeR spliceVariants
+
+spliceVariant.DS <- function(raw.counts, feature_association, factors) {
+  library(edgeR)
+  tmm_factors = calcNormFactors(raw.counts)
+  DGEList_object_counts = DGEList(counts = round(raw.counts),
+                                  lib.size = colSums(raw.counts),
+                                  group = as.factor(factors[,"Replicate"]), genes = NULL,
+                                  norm.factors = tmm_factors,
+                                  remove.zeros = FALSE)
+  isoform_names <- row.names(feature_association)
+  feature_association <- as.data.frame(feature_association)
+  row.names(feature_association) <- isoform_names
+
+  results = spliceVariants(DGEList_object_counts, geneID=as.character(feature_association[rownames(feature_association) %in% rownames(raw.counts),"id"]),
+                           dispersion=NULL, estimate.genewise.disp = TRUE)$table[,"PValue",drop=F]
+  isoPerGene = table(feature_association["id"])
+  genesMoreOneIso = isoPerGene[isoPerGene>1]
+  results_multi = results[names(genesMoreOneIso),,drop=F]
+  return(results_multi)
+}
+
+### DEXseq
+
+DEXSeq.DS <- function(raw.counts, feature_association, factors) {
+  library(DEXSeq)
+  sample_table <- data.frame(row.names = colnames(raw.counts),
+                             condition = as.factor(factors[,1]))
+  dxd <- DEXSeqDataSet(round(raw.counts), sampleData = sample_table, design = ~ sample + exon + condition:exon,
+                       featureID = rownames(raw.counts),
+                       groupID = as.character(feature_association[rownames(raw.counts),"id"]))
+  tmm_librarySize_factors = tmm_factors_function(raw.counts, factors)
+  sizeFactors(dxd) = c(tmm_librarySize_factors, tmm_librarySize_factors)
+  ## pass number of workers? - 2 minimum: , BPPARAM = MulticoreParam(workers = 4))
+  #dxd <- estimateDispersions(dxd, BPPARAM = MulticoreParam(workers = 4))
+  if (.Platform$OS.type == "unix") {
+    bp_param <- MulticoreParam(workers=4);
+  } else if (.Platform$OS.type == "windows") {
+    bp_param <- SnowParam(workers=4);
+  }
+  dxd <- estimateDispersions(dxd, BPPARAM = bp_param);
+  ## Run the test and get results for exon bins
+  dxd <- testForDEU(dxd)
+  ## Summarizing results on gene level
+  res <- DEXSeqResults(dxd)
+  print(dim(res))
+  print(head(res))
+  pgq <- perGeneQValue(res, p = "pvalue")
+  results <- data.frame(row.names = names(pgq), q.value = pgq)
+  return(results)
+}
+
+
+#### Calculation of TMM normalization factors
+
+tmm_factors_function <- function(counts, myfactors) {
+  library(edgeR)
+  myedgeR = DGEList(counts = counts, group = myfactors[,1])
+  myedgeR = calcNormFactors(myedgeR, method = "TMM", refColumn = 1)
+  norm_factors_edgeR =  myedgeR$samples$norm.factors
+  total = colSums(as.matrix(counts))
+  norm_factors_edgeR = norm_factors_edgeR * (total/mean(total))
+  return(norm_factors_edgeR)
+}
+
+#### Shift in the position of isoforms by level of expression
+
+podiumChange <- function(norm.counts, feature_association, myfactors) {
+
+  isoform_names <- row.names(norm.counts)
+  norm.counts <- as.data.frame(norm.counts)
+  row.names(norm.counts) <- isoform_names
+
+  isoform_names <- row.names(feature_association)
+  feature_association <- as.data.frame(feature_association)
+  row.names(feature_association) <- isoform_names
+
+  as.character(feature_association[rownames(norm.counts),"id"])
+
+  factors = split(myfactors, myfactors[,"Replicate"])
+  norm.counts_mean = data.frame(cond1=apply(norm.counts[,rownames(factors[[1]])], 1, function(x) mean(x)),
+                                cond2=apply(norm.counts[,rownames(factors[[2]])], 1, function(x) mean(x)),
+                                gene=feature_association[rownames(norm.counts),"id"])
+  changeOfPosition = as.list(by(norm.counts_mean[,c(1,2)], norm.counts_mean[,"gene"], function(x) {
+    prop = t(t(x)/colSums(x))*100
+    prop[is.nan(prop)] <- 0
+    prop = as.data.frame(prop)
+    position = data.frame(cond1_pos = order(prop$cond1, decreasing = TRUE), cond2_pos = order(prop$cond2, decreasing = TRUE), delta=abs(prop$cond1-prop$cond2))
+    position <<- position
+    if (position[1,"cond1_pos"] != position[1,"cond2_pos"]) { r = TRUE }
+    else {r = FALSE}
+    return (list(podiumChange = r, totalChange = sum(position$delta)/2))
+  }))
+  A = unlist(sapply(changeOfPosition, function(x) x$podiumChange))
+  B = unlist(sapply(changeOfPosition, function(x) x$totalChange))
+  return(list(podiumChange=A, totalChange=B))
+}
---
+++ differential_analysis/subset_and_run_DIU.py
@@ -0,0 +1,116 @@
+#!/usr/bin/env python
+
+import os
+import os.path
+import subprocess
+import pandas as pd
+import argparse
+from argparse import Namespace
+import distutils.spawn
+import sys
+
+old_stdout = sys.stdout
+
+RSCRIPTPATH = distutils.spawn.find_executable('Rscript')
+RSCRIPT_DIU = "/gpfs/mrc0/projects/Research_Project-MRC148213/sl693/scripts/LOGen/differential_analysis/run_DIU_commandLine.R"
+# include rownames in classification file
+
+if os.system(RSCRIPTPATH + " --version")!=0:
+    print("Rscript executable not found! Abort!", file=sys.stderr)
+    sys.exit(-1)
+
+
+def generate_gene_specific_files(args, gene):
+
+    print("Generating gene-specific files:", gene)
+    print("Writing output files to:", args.dir)
+
+    # extract the associated isoform
+    with open(args.classFile, 'r') as file:
+        first_line = file.readline()  # Read the first line
+        with open(args.dir + gene + '_classification.txt', 'w') as test1_file:  # Open in write mode to clear the file
+            test1_file.write(first_line)
+
+        with open(args.dir + gene + '_isoform.txt', 'w') as test2_file:  # Open in write mode to clear the file
+            test2_file.write("")  # You can write an empty string or any initial content here
+
+        for line in file.readlines()[1:]:  # Skip the first line
+            associated_gene = line.split("\t")[first_line.split("\t").index("associated_gene") + 1]
+            isoform = line.split("\t")[1]
+            if associated_gene == gene:
+                with open(args.dir + gene + '_classification.txt', "a") as myfile:
+                    myfile.write(line + "\n")
+                with open(args.dir + gene + '_isoform.txt', "a") as myfile:
+                    myfile.write(isoform + "\n")
+
+    # sanity check there are isoforms in the normalized expression file
+    if os.stat(args.dir + gene + "_classification.txt").st_size == 0:
+       print("ERROR: no isoforms subset in expression file")
+       sys.exit()
+
+    # note isoforms that are in the classfile might not be in the normalise expression file
+    print("Extract associated isoforms from normalised expression file")
+    grepcmd = "grep -f {f} {n} > {o}".format(f=args.dir+gene+'_isoform.txt',n=args.normExp,o=args.dir+gene+'_normalised_expression.txt')
+    sedcmd = "sed  -i '1i isoform,sample,normalised_counts' {f}".format(f=args.dir+gene+'_normalised_expression.txt')
+    subprocess.run(grepcmd, shell=True)
+    subprocess.run(sedcmd,shell=True)
+
+    if os.stat(args.dir + gene + "_normalised_expression.txt").st_size == 0:
+       print("ERROR: no isoforms subset in expression file")
+       subprocess.run("rm {f}".format(f=args.dir+gene+'_normalised_expression.txt'), shell=True)
+       sys.exit()
+
+
+
+def run_DIU(args, gene):
+    runRDIU = RSCRIPTPATH + " {s} -c {d}/{g}{c} -e {d}{g}{e} -t {t} -p {p} -f {f} &> {d}/{g}_r.log ".format(s=RSCRIPT_DIU,c='_classification.txt',e='_normalised_expression.txt',t=args.name,d=args.dir,g=gene,p=args.phenotype,f=args.factor)
+    subprocess.run(runRDIU, shell=True)
+
+
+def main():
+    parser = argparse.ArgumentParser(description="Subset fasta and gtf from list of ID")
+    parser.add_argument('classFile', help='\t\tSQANTI classification file (unfiltered)')
+    parser.add_argument('normExp', help='\t\tNormalised expression matrix')
+    parser.add_argument('-i', '--id_list', required=False, help='\t\tTxt file of list of ID to subset, no rownames and colnames')
+    parser.add_argument('-I', '--ID_list', required=False, nargs='+', help='\t\tlist of ID to subset - direct on command line delimited by ,')
+    parser.add_argument('-p', '--phenotype', help='\t\tInput phenotype file for downstream DIU analysis')
+    parser.add_argument('-f', '--factor', help='\t\tInput factor file for downstream DIU analysis')
+    parser.add_argument('-n', '--name', help='\t\tcommon character in expression columns to capture from classification file')
+    parser.add_argument('-d','--dir', required=False, help='\t\tDirectory for output files. Default: Directory of input bed.')
+
+
+    args = parser.parse_args()
+
+    if args.dir is None:
+       if not os.path.dirname(args.classFile):
+         # If it's empty, set it to the current directory
+          args.classFile = os.path.join(os.getcwd(), args.classFile)
+
+       args.dir = os.path.dirname(args.classFile) + "/"
+    else:
+       args.dir = args.dir + "/"
+
+    if args.id_list is not None:
+        print("Reading in", args.id_list)
+        id_list = pd.read_csv(args.id_list, sep = "\t", header = None)
+        genes = list(id_list[0].values)
+    elif args.ID_list is not None:
+        genes = [i.split(",") for i in args.ID_list][0]
+    else:
+        print("Error: need to include --id_list or --ID_list")
+        sys.exit()
+
+
+    for g in genes:
+        log_file = open(args.dir + g + "_py.log","w")
+        sys.stdout = log_file
+
+        generate_gene_specific_files(args, g)
+        run_DIU(args,g)
+
+        sys.stdout = old_stdout
+        log_file.close()
+
+
+if __name__ == "__main__":
+    main()
