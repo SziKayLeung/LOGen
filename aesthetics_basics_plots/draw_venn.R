a4e71026bd9e482b9eb76f617109995040c01c76
sl693 <sl693@exeter.ac.uk>
2024-01-30 15:08:29 +0000

    Aesthetic themes modified - twovenndiagrams output - Change font to Arial from CM Roman - add plot_break_every_nth() - add number-ticks()

--- aesthetics_basics_plots/draw_venn.R
+++ aesthetics_basics_plots/draw_venn.R
@@ -4,9 +4,10 @@ futile.logger::flog.threshold(futile.logger::ERROR, name = "VennDiagramLogger")
 
 twovenndiagrams <- function(set1, set2, name1, name2){
   p <- venn.diagram(x = list(set1,set2), 
-                    category.names = c(name1,name2),filename = NULL, output=TRUE, lwd = 0.2,lty = 'blank', 
-                    fill = c("#B3E2CD", "#FDCDAC"), main = "\n", cex = 1,fontface = "bold",fontfamily = "ArialMT",#
-                    cat.cex = 1,  cat.default.pos = "outer",  cat.pos = c(-120, 120), cat.dist = c(0.055, 0.055),  cat.fontfamily = "ArialMT",  #rotation = 1,   main = "\n\n\n\n"
+                    label_alpha = 0, category.names = c(name1,name2),filename = NULL, output=TRUE, lwd = 0.2,lty = 'blank', 
+                    fill = c("#B3E2CD", "#FDCDAC"), main = "\n", cex = 1,fontface = "bold",fontfamily = "ArialMT",
+                    cat.cex = 1,  cat.default.pos = "outer",  cat.col = c("#60756c", "#ba7443"),
+                    cat.pos = c(-145, 200), cat.dist = c(-0.15,-0.03),  cat.fontfamily = "ArialMT",  #rotation = 1,   main = "\n\n\n\n"
                     print.mode = "raw")
   return(p)
 }
--- aesthetics_basics_plots/pthemes.R
+++ aesthetics_basics_plots/pthemes.R
@@ -1,8 +1,12 @@
 
 suppressMessages(library(viridis)) 
+suppressMessages(library(ggplot2)) 
 suppressMessages(library(wesanderson)) 
 suppressMessages(library(extrafont))
 suppressMessages(loadfonts())
+
+# negate
+`%ni%` <- Negate(`%in%`)
 
 # plot aesthetics
 mytheme <- theme(axis.line = element_line(colour = "black"),
@@ -10,30 +14,52 @@ mytheme <- theme(axis.line = element_line(colour = "black"),
                  panel.grid.minor = element_blank(),
                  panel.border = element_blank(),
                  panel.background = element_blank(),
-                 text=element_text(size=18,  family="CM Roman"),
+                 text=element_text(size=16),
                  axis.title.x = element_text(vjust=-0.5, colour = "black"),
                  axis.title.y = element_text(vjust=0.5, margin = margin(t = 0, r = 10, b = 0, l = 0)),
                  legend.position = c(.90, 0.95),
-                 #legend.justification = c(1,1),
                  legend.box.just = "right",
                  legend.margin = margin(6, 6, 6, 6),
-                 legend.text = element_text(size = 18,family="CM Roman"),
-                 axis.text.x= element_text(size=16,family="CM Roman"),
-                 axis.text.y= element_text(size=16,family="CM Roman"),
-                 plot.title = element_text(size=16))
+                 legend.text = element_text(size = 12),
+                 axis.text.x= element_text(size=16),
+                 axis.text.y= element_text(size=16),
+                 plot.title = element_text(size=16),
+                 plot.subtitle = element_text(size=14))
+
+mythemeNoLegend <- theme(axis.line = element_line(colour = "black"),
+                 panel.grid.major = element_blank(),
+                 panel.grid.minor = element_blank(),
+                 panel.border = element_blank(),
+                 panel.background = element_blank(),
+                 text=element_text(size=16),
+                 axis.title.x = element_text(vjust=-0.5, colour = "black"),
+                 axis.title.y = element_text(vjust=0.5, margin = margin(t = 0, r = 10, b = 0, l = 0)),
+                 legend.text = element_text(size = 16),
+                 axis.text.x= element_text(size=16),
+                 axis.text.y= element_text(size=16),
+                 plot.title = element_text(size=16),
+                 plot.subtitle = element_text(size=14))
+
 
 
 # subplot aesthetics
-mytheme_font <- theme(text=element_text(size=18,  family="CM Roman"),
+mytheme_font <- theme(text=element_text(size=16),
                       axis.title.x = element_text(vjust=-0.5, colour = "black"),
                       axis.title.y = element_text(vjust=0.5, margin = margin(t = 0, r = 10, b = 0, l = 0)),
-                      legend.text = element_text(size = 12,family="CM Roman"),
-                      axis.text.x= element_text(size=16,family="CM Roman"),
-                      axis.text.y= element_text(size=16,family="CM Roman"))
+                      legend.text = element_text(size = 12),
+                      axis.text.x= element_text(size=16),
+                      axis.text.y= element_text(size=16))
 
-# scale axis into 1000s
+# scale axis into 1000s 
+# e.g. scale_y_continuous(labels = ks) 
 ks <- function(x){ format(x/1000, big.mark=",")} 
 
 # change x axis
 perc_lab <- function(x){ format(x* 100, big.mark=",")} 
 
+# ticks for x axis
+number_ticks <- function(n) {function(limits) pretty(limits, n)}
+
+# use for plotting (scale_x_discrete) to determine how many breaks to be used
+# e.g scale_x_discrete(breaks = every_nth(n = 2)) 
+plot_break_every_nth <- function(n) {return(function(x) {x[c(TRUE, rep(FALSE, n - 1))]})}
